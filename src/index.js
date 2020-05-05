6ac530c5dd6d9e8a4b23b2c0b74ae0d06b6ea0b6
Jakapob <jakapobn@gmail.com>
2020-05-05 12:41:02 +0700

    validate redux form

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 
 import App from './components/App';
 import reducers from './reducers';
 import logger from 'redux-logger'
 
-const store = createStore(reducers, applyMiddleware(logger));
+const composeEnchancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+// const store = createStore(reducers, applyMiddleware(logger));
+const store = createStore(reducers,  composeEnchancers(applyMiddleware()));
 
 ReactDOM.render(
     <Provider store={store}>
